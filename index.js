680347923363831fc5dfd024eab22f157181b5d7
Ananya Maity <uxboy95@gmail.com>
2025-02-06 17:27:27 +0530

    Updated code and dependencies

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-const { faker } = require('@faker-js/faker');
+import { faker } from '@faker-js/faker';
+import mysql from 'mysql2/promise';
+const connection = await mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'delta_app',
+  password: '26674870',
+});
+// A simple SELECT query
+try {
+  const [results, fields] = await connection.query(
+    'SHOW TABLES'
+  );
+
+  console.log(results); // results contains rows returned by server
+  console.log(fields); // fields contains extra meta data about results, if available
+} catch (err) {
+  console.log(err);
+}
+
+
 const createRandomUser = () => {
     return {
       id: faker.string.uuid(),
@@ -7,4 +27,4 @@ const createRandomUser = () => {
       password: faker.internet.password(),
     };
   }
-  console.log(createRandomUser());
+  // console.log(createRandomUser());
