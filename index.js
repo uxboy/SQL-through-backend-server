44d3ee9c345fc321d9442f335f6ec7bb5c4e94cd
Ananya Maity <uxboy95@gmail.com>
2025-02-11 23:15:30 +0530

    Added index.js and package files

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+import express from 'express'; // ✅ Use ES Module syntax
+
+const app = express();
+const port = 8080;
+
 import { faker } from '@faker-js/faker';
 import mysql from 'mysql2/promise';
 const connection = await mysql.createConnection({
@@ -7,9 +12,6 @@ const connection = await mysql.createConnection({
   password: '26674870',
 });
 // inserting new data
-let q = 'INSERT INTO user (id, username, email, password) VALUES ?';
-
-let data = [];
 
 const getRandomUser = () => {
   return [
@@ -19,17 +21,19 @@ const getRandomUser = () => {
     faker.internet.password(),
   ];
 }
-for(let i=1;i<=100;i++){
-  data.push(getRandomUser());
-}
+// connection.end();
 
-
-try {
-  const [results, fields] = await connection.query(q, [data]); // Pass users as a single array inside another array
-  console.log(results);
+app.get("/",async (req,res)=>{
+  let q = 'SELECT COUNT(*) FROM user;';
+  try {
+  const [results, fields] = await connection.query(q); 
+  console.log(results[0]['COUNT(*)']);
+  res.send(String(results[0]['COUNT(*)']));
 } catch (err) {
   console.error(err);
+  res.status(500).send("Internal Server Error");
 }
-
-connection.end();
-
+})
+app.listen(port , ()=>{
+  console.log(`app is listening to port ${port}`);
+})
