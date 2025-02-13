5a4de8df84c116f63256ef2a0757911bb3553033
Ananya Maity <uxboy95@gmail.com>
2025-02-14 00:28:04 +0530

    Added/Updated views

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 import express from 'express'; // ✅ Use ES Module syntax
-
 const app = express();
 const port = 8080;
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+import path from 'path';
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 import { faker } from '@faker-js/faker';
 import mysql from 'mysql2/promise';
@@ -21,14 +28,39 @@ const getRandomUser = () => {
     faker.internet.password(),
   ];
 }
+// let q = "INSERT INTO user (id, username, email, password) VALUES ?;";
+// let data = [];
+// for(let i=1;i<=100;i++){
+//   data.push(getRandomUser());
+//   console.log(data)
+// }
+// try {
+//   const [results] = await connection.query(q,[data]);
+//   console.log(results);
+// } catch (err) {
+//   console.log(err);
+// }
 // connection.end();
 
 app.get("/",async (req,res)=>{
   let q = 'SELECT COUNT(*) FROM user;';
   try {
   const [results, fields] = await connection.query(q); 
-  console.log(results[0]['COUNT(*)']);
-  res.send(String(results[0]['COUNT(*)']));
+  let count = results[0]['COUNT(*)'];
+  res.render("home.ejs",{ count });
+} catch (err) {
+  console.error(err);
+  res.status(500).send("Internal Server Error");
+}
+})
+app.get("/user",async (req,res)=>{
+  let q = 'SELECT * FROM user;';
+  try {
+  const [results, fields] = await connection.query(q); 
+  console.log(results);
+  let peoples = results;
+  res.render("users.ejs" , {peoples});
+  // res.send(results);
 } catch (err) {
   console.error(err);
   res.status(500).send("Internal Server Error");
